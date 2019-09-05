21f9fb96a71a2b88766a3b1ddc010a16f2a59f4d
pravee2667 <52732473+pravee2667@users.noreply.github.com>
2019-09-05 12:41:41 +0530

    changed the app file by removing

--- app.py
+++ app.py
@@ -68,12 +68,15 @@ from matplotlib.figure import Figure
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 from werkzeug import secure_filename
 #import pyLDAvis.gensim
-import topic_mode
+#import topic_mode
 import pandas as pd
-import connections
+#import connections
+#import os
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+#import extracttable
 
 app = Flask(__name__)
-result=connections.connect()
+#result=connections.connect()
 
 
 @app.route('/home')
@@ -90,45 +93,131 @@ def login():
             return redirect(url_for('hello_world'))
     return render_template('login.html', error=error)
 
-@app.route('/topicmodelling')
-def topic_mod():
-    return render_template("home.html")
+#@app.route('/topicresult',methods=['POST','GET'])
+#def topic_mod():
+#    if request.method=="POST":
+#        #return response
+#        
+
+@app.route('/pact_scanhome')
+def pact_scanhome():
+    return render_template("landing1.html")    
+
+#@app.route('/topicmodelling')
+#def topic_result():
+#    result=connections.pull_collections()
+#    return render_template("home.html",options=result)
+#
+#@app.route('/sentiment_input',methods=['POST','GET'])
+#def sentiment_input():
+#    #a=['blue','green','grey']
+#    result=connections.pull_collections()
+#    return render_template("sentiment_input.html",options=result)
+
+#@app.route('/sentiment_cosmos',methods=['POST','GET'])
+#def senti_cosmos():
+#    if request.method=="POST":
+#        select_val=request.form['containers']
+#        result="Sorry ! Data is empty in " + select_val + "Container"
+##        result=connections.connect(select_val)
+##        target=[]
+##        sia=SentimentIntensityAnalyzer()
+##        for i in result:
+##            #print(i)
+##            polarity=sia.polarity_scores(i)
+##            #print(polarity)
+##            target.append(max(polarity,key=lambda x : polarity[x]))
+##            #print(target)
+##        target=['Positive','Positive','Negative','Positive']
+##        sent_df=pd.DataFrame(list(zip(result,target)))
+##    
+##        response=make_response(sent_df.to_csv())
+##        response.headers["Content-Disposition"] = "attachment; filename=export.csv"
+##        response.headers["Content-Type"] = "text/csv"
+#        return render_template("output.html",output=result)
+        
+#@app.route('/topic_cosmos',methods=['POST','GET'])
+#def topic_cosmos():
+#    if request.method=="POST":
+#        select_val=request.form['containers']
+#        result="Sorry ! Data is empty in " + select_val + "Container"
+##        result=connections.connect(select_val)
+##        ldamodel,corpus=topic_mode.topic_modell(result)
+##        df=format_topics_sentences(ldamodel,corpus,result)
+#        #df.save(os.path.join("/tmp/","topic.csv"))
+#        #df1=df.head(3)
+##        response=make_response(df.to_csv())
+##        response.headers["Content-Disposition"] = "attachment; filename=export.csv"
+##        response.headers["Content-Type"] = "text/csv"
+#        return render_template("output.html",output=result)
+        
+    
 
 
-@app.route('/topic',methods=['POST','GET'])
-def topic_modelling():
+@app.route('/sentiment_output',methods=['POST','GET'])
+def sentiment_output():
     if request.method=="POST":
         f=pd.read_csv(request.files.get('file'))
-        #ff=secure_filename(f.filename)
-        ldamodel,corpus=topic_mode.topic_modell(f)
-        df=format_topics_sentences(ldamodel,corpus,f['Articles'])
-        response=make_response(df.to_csv())
-        response.headers["Content-Disposition"] = "attachment; filename=export.csv"
-        response.headers["Content-Type"] = "text/csv"
-        return response
+        target=[]
+        sia=SentimentIntensityAnalyzer()
+        for i in f['Articles']:
+            #print(i)
+            polarity=sia.polarity_scores(i)
+            #print(polarity)
+            target.append(max(polarity,key=lambda x : polarity[x]))
+            #print(target)
+        target=['Positive','Positive','Negative','Positive']
+        sent_df=pd.DataFrame(list(zip(f['Articles'],target)))
+        sent_df.columns=["Text","Sentiment_result"]
+    
+#        response=make_response(sent_df.to_csv())
+#        response.headers["Content-Disposition"] = "attachment; filename=export.csv"
+#        response.headers["Content-Type"] = "text/csv"
+        return render_template("sentiment_output.html",result=sent_df.to_html())
+        
+    
 
-def format_topics_sentences(ldamodel, corpus, texts):
-    # Init output
-    sent_topics_df = pd.DataFrame()
-    # Get main topic in each document
-    for i, row_list in enumerate(ldamodel[corpus]):
-        row = row_list[0] if ldamodel.per_word_topics else row_list            
-        # print(row)
-        row = sorted(row, key=lambda x: (x[1]), reverse=True)
-        # Get the Dominant topic, Perc Contribution and Keywords for each document
-        for j, (topic_num, prop_topic) in enumerate(row):
-            if j == 0:  # => dominant topic
-                wp = ldamodel.show_topic(topic_num)
-                topic_keywords = ", ".join([word for word, prop in wp])
-                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
-            else:
-                break
-    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
-    # Add original text to the end of the output
-    contents = pd.Series(texts)
-    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
-    return(sent_topics_df)
 
+#@app.route('/topic',methods=['POST','GET'])
+#def topic_modelling():
+#    if request.method=="POST":
+#        #file=request.files['file']
+#        f=pd.read_csv(request.files.get('file'))
+#        #filename=secure_filename(file.filename)
+#        
+#        
+#        ldamodel,corpus=topic_mode.topic_modell(f)
+#        df=format_topics_sentences(ldamodel,corpus,f['Articles'])
+#        #df.save(os.path.join("/tmp/","topic.csv"))
+#        #df1=df.head(3)
+##        response=make_response(df.to_csv())
+##        response.headers["Content-Disposition"] = "attachment; filename=export.csv"
+##        response.headers["Content-Type"] = "text/csv"
+#        return render_template("topic_output.html",result=df.to_html())
+#
+#def format_topics_sentences(ldamodel, corpus, texts):
+#    # Init output
+#    sent_topics_df = pd.DataFrame()
+#    # Get main topic in each document
+#    for i, row_list in enumerate(ldamodel[corpus]):
+#        row = row_list[0] if ldamodel.per_word_topics else row_list            
+#        # print(row)
+#        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+#        # Get the Dominant topic, Perc Contribution and Keywords for each document
+#        for j, (topic_num, prop_topic) in enumerate(row):
+#            if j == 0:  # => dominant topic
+#                wp = ldamodel.show_topic(topic_num)
+#                topic_keywords = ", ".join([word for word, prop in wp])
+#                topic="Topic_"+str(topic_num)
+#                sent_topics_df = sent_topics_df.append(pd.Series([topic_keywords,topic, round(prop_topic*100,2)]), ignore_index=True)
+#            else:
+#                break
+#    sent_topics_df.columns = ['Topic_Keywords','Dominant_Topic', 'Perc_Contribution' ]
+#    # Add original text to the end of the output
+#    contents = pd.Series(texts)
+#    sent_topics_df = pd.concat([contents,sent_topics_df], axis=1)
+#    return(sent_topics_df)
+#
 
 
 if __name__ == "__main__":
